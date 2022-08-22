6b9bdfc5463dd538632b36e2dd5e716f1dc41e9a
ASWATH48 <kingaswath46@gmail.com>
2022-08-22 14:35:58 +0530

    updated quiz features

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,20 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+    def is_admin
+        @admin_log = User.find(session[:current_user])
+        @is_exist = false
+         if @admin_log.username == "admin123"
+              @is_exist = true
+         end
+         @is_exist
+      end
+      def user_exists
+        @user_log = User.find(session[:current_user])
+        @exists = false
+         if @auser_log
+              @exists = true
+         end
+         @exists
+      end
 end
--- app/controllers/quiz_controller.rb
+++ app/controllers/quiz_controller.rb
@@ -1,108 +1,173 @@
 class QuizController < ApplicationController
-    @@ary = []
-    @@qno_param = []
-    def quiz
-        
-    end
-    def quiz_index
+
+  @@ary = []
+
+  @@qno_param = []
+
+
+  def quiz
+
+  end
+
+
+  def quiz_index
+
+  end
+
+
+  def is_admin
+
+    @admin_log = User.find(session[:current_user])
+
+    if @admin_log.id == 1
+
+      @is_exist = true
+
+    else
+
+      @is_exist = false
 
     end
-    def is_admin
-      @admin_log = User.find(session[:current_user])
-       if @admin_log.id == 1
-            @is_exist = true
-        else
-         @is_exist = false
-       end
-      p @is_exist
+
+    p @is_exist
+
+  end
+
+
+  def question
+
+    if params[:id] != nil
+
+      @@ary.insert(0, params[:id])
+
     end
-    def question
-       if params[:id] != nil
-         @@ary.insert(0 ,params[:id])
-       end
-       @ids = @@ary[0]
-       p "============================================"
-       p "params id"
-       p params[:id] != nil
-       p @ids
-       p @@ary
-      #  p params[:id]
-       @ans = Quest.where(assesments_id: @@ary[0])
-       p "ans_id"
-       p @ans
+
+    @ids = @@ary[0]
+
+    p "============================================"
+
+    p "params id"
+
+    p params[:id] != nil
+
+    p @ids
+
+    p @@ary
+
+    #  p params[:id]
+
+    @ans = Quest.where(assesments_id: @@ary[0])
+
+    p "ans_id"
+
+    p @ans
+
 
 
     # question no calculation
-       @question_no = Quest.where(assesments_id: @ids)
-       @questions = []
-       @question_no.all.map do |quest| 
-        @questions.push(quest.id)
-       end
-       p "============================================"
-       p "question arr"
-       p @questions
-       @questions
-       
-      # find question no
-          @temp_qno = Quest.find_by(params[:qno])
-          @temp_id = Quest.find_by(assesments_id: @ids)
 
-        if @ids != nil
-            if @temp_qno.present?
-               @@qno_param.insert(0, params[:qno]) 
-               p "hi"                
-            elsif @temp_id != nil
-                  @@qno_param.insert(0 ,@temp_id.id )
-               p "bye" 
-               end
-         elsif params[:qno] != nil
-            if @temp_qno.nil?
-               @@qno_param.insert(0, params[:qno]) 
-               p "hello"
-            else
-               @@qno_param.insert(0 ,@temp_id.id )
-               p "hi1"  
-            end 
-       end
-       @q_id = @@qno_param[0]
-      #  p "++++++++"
-       p  params[:qno] != nil
-       p @q_id
-      p "=================is admin "
-      p is_admin
+    @question_no = Quest.where(assesments_id: @ids)
+
+    @questions = []
+
+    @question_no.all.map do |quest|
+
+      @questions.push(quest.id)
+
     end
-      def create_quiz
-      @assesment = Assesment.new(params.require(:assesment).permit(:title, :user_id ))
-      @assesment[:status] = false
-            if @assesment.save
-               render "quiz_index"
-            end
-      end
-      def delete_quiz
-      @delete_quiz = Assesment.find_by(id: params[:delquiz_id])
-      if @delete_quiz.destroy
-         redirect_to "/quiz/home"
-      end
+
+    p "============================================"
+
+    p "question arr"
+
+    p @questions
+
+    @questions
+
+
+
+    # find question no
+
+    @temp_qno = Quest.find_by(params[:qno])
+
+    @temp_id = Quest.find_by(assesments_id: @ids)
+
+
+
+   if @ids != nil
+
+      if @temp_qno.present?
+        @@qno_param.insert(0, params[:qno])
+      elsif @temp_id != nil
+        @@qno_param.insert(0, @temp_id.id)
       end
 
-       def delete_quest
-       @del_id = Quest.find(params[:del_id])
-       if @del_id.destroy
-          @@qno_param = []
-          redirect_to "/quiz/question"
-       end
-
-       p"========================================================="
-       p params[:del_id]
-       end
-
-      def create_quest
-         @quest = Quest.new(params.require(:quest).permit(:question, :answer , :choice1, :choice2 , :choice3 , :user_id ,:assesments_id))
-         if @quest.save
-               @ids = @@ary[0]
-               redirect_to "/quiz/question"
+   elsif params[:qno] != nil
+         if @temp_qno.nil?
+            @@qno_param.insert(0, params[:qno])
+         else
+            @@qno_param.insert(0, @temp_id.id)
          end
-      end
+    end
+    @q_id = @@qno_param[0]
+  end
 
-   end
 
+  def create_quiz
+
+    @assesment = Assesment.new(params.require(:assesment).permit(:title, :user_id))
+    @assesment[:status] = false
+       if @assesment.save
+            render "quiz_index"
+       end
+
+  end
+
+
+
+  def delete_quiz
+
+    @delete_quiz = Assesment.find_by(id: params[:delquiz_id])
+    if @delete_quiz.destroy
+      redirect_to "/quiz/home"
+    end
+
+  end
+
+
+
+  def delete_quest
+    @del_id = Quest.find(params[:del_id])
+    Answer.find_by(quests_id: @del_id).destroy
+    if @del_id.destroy
+      @@qno_param = []
+
+      redirect_to "/quiz/question"
+
+    end
+
+
+
+    p "========================================================="
+
+    p params[:del_id]
+
+  end
+
+
+
+  def create_quest
+
+    @quest = Quest.new(params.require(:quest).permit(:question, :answer, :choice1, :choice2, :choice3, :user_id, :assesments_id))
+
+    if @quest.save
+
+      @ids = @@ary[0]
+
+      redirect_to "/quiz/question"
+
+    end
+
+  end
+
+end
